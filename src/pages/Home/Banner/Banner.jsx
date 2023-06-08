f425c7bab7cc7c844aa001ea14de88d76e80ae6c
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-06-09 02:51:49 +0600

    display different menu category items with cover and and some slider img updates on home page

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,45 +1,42 @@
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-
-
-
-import img1 from '../../../assets/chhola.jpg'                
-import img2 from '../../../assets/dosa.jpg'                
-import img3 from '../../../assets/gujrati.jpeg'                 
-import img4 from '../../../assets/idli.jpg'                 
-import img5 from '../../../assets/masala.jpg'                 
-import img6 from '../../../assets/paneer.jpg'                 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from '../../../assets/home/01.jpg'
+import img2 from '../../../assets/home/02.jpg'
+import img3 from '../../../assets/home/03.png'
+import img4 from '../../../assets/home/04.jpg'
+import img5 from '../../../assets/home/05.png'
+import img6 from '../../../assets/home/06.png'
 
 const Banner = () => {
     return (
         <Carousel>
             <div>
                 <img src={img1} alt="" />
-               
+
             </div>
             <div>
                 <img src={img2} alt="" />
-               
+
             </div>
             <div>
                 <img src={img3} alt="" />
-               
+
             </div>
             <div>
                 <img src={img4} alt="" />
-               
+
             </div>
             <div>
                 <img src={img5} alt="" />
-               
+
             </div>
             <div>
                 <img src={img6} alt="" />
-               
+
             </div>
 
         </Carousel>
-      
+
     );
 };
 
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,10 +1,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
-import slide1 from '../../../assets/chhola.jpg'
-import slide2 from '../../../assets/chhola.jpg'
-import slide3 from '../../../assets/chhola.jpg'
-import slide4 from '../../../assets/chhola.jpg'
-import slide5 from '../../../assets/chhola.jpg'
+import slide1 from '../../../assets/home/slide1.jpg'
+import slide2 from '../../../assets/home/slide2.jpg'
+import slide3 from '../../../assets/home/slide3.jpg'
+import slide4 from '../../../assets/home/slide4.jpg'
+import slide5 from '../../../assets/home/slide5.jpg'
 
 
 import "swiper/css";
@@ -15,9 +15,9 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 const Category = () => {
     return (
         <section>
-            <SectionTitle 
-            subHeading={"From 11.am to 10.00pm"}
-            heading={"Order Online"}>
+            <SectionTitle
+                subHeading={"From 11.am to 10.00pm"}
+                heading={"Order Online"}>
 
             </SectionTitle>
             <Swiper
