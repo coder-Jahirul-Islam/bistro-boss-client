f623bad4bed23e41751615f2e847f9fa36612b9e
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-07-24 01:47:31 +0600

    Delete cart item and sync data using refetch

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,19 +1,94 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+
+
 
 const MyCart = () => {
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     console.log(cart);
-    const total = cart.reduce((sum, item) => item.price + sum, 0)
+    // how does reduce work!!!?
+    // const total = cart.reduce((sum, item) => item.price + sum, 0);
+    const total = cart.reduce((sum, item) => sum + parseFloat(item.price), 0).toFixed(2);
+
+    const handleDelete = (item) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/carts/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your cart has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+
+    }
     return (
-        <div>
+        <div className="w-[60%]">
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
-            <div className="uppercase">
+            <div className="uppercase font-semibold flex h-[60px] justify-evenly items-center">
                 <h3 className="text-3xl">Total items: {cart.length}</h3>
-                <h3 className="text-3xl">Total price: {total}</h3>
+                <h3 className="text-3xl">Total price: ${total}</h3>
                 <button className="btn btn-warning btn-sm">pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart.map((item, index) => <tr
+                            key={item._id}
+                        >
+                            <td>
+                                {index + 1}
+                            </td>
+                            <td>
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={item.image} />
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {item.name}
+                            </td>
+                            <td className="font-semibold text-end">${item.price}</td>
+                            <td>
+                                <button onClick={() => handleDelete(item)} className="btn btn-ghost text-white bg-red-500 btn-md"><FaTrashAlt></FaTrashAlt></button>
+                            </td>
+                        </tr>)}
+
+                    </tbody>
+                </table>
             </div>
 
         </div>
